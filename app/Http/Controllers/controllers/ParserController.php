d809f01afad386fbf30e4fb22f8094cc5731f968
vladimir-pitchkurov <vladimir.pitchkurov@gmail.com>
2018-08-29 00:19:42 +0300

    create ParserController

---
+++ app/Http/Controllers/controllers/ParserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ParserController extends Controller
+{
+    public function index()
+    {
+        echo "Hello from ParserController!";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/parser', ['uses' => 'controllers\ParserController@index']);
