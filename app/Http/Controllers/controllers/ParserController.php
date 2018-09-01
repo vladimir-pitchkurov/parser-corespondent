6fe64f812106205f343cea0d93b589a4d6240988
vladimir-pitchkurov <vladimir.pitchkurov@gmail.com>
2018-09-02 01:06:36 +0300

    some simple refactoring

--- app/Http/Controllers/controllers/ParserController.php
+++ app/Http/Controllers/controllers/ParserController.php
@@ -2,87 +2,50 @@
 
 namespace App\Http\Controllers\controllers;
 
+use App\Drivers\KorespondentDriver;
+use App\Helpers\DbHelper;
+use App\models\Course;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Symfony\Component\DomCrawler\Crawler as DomCrawler;
-use GuzzleHttp\Exception\GuzzleException;
-use GuzzleHttp\Client as GuzzleClient;
+
 
 class ParserController extends Controller
 {
+    protected $driver;
+    protected $helper;
+
+    public function __construct(KorespondentDriver $driver, DbHelper $helper)
+    {
+        $this->driver = new KorespondentDriver();
+        $this->helper = $helper;
+    }
+
     public function index()
     {
         //$link = "https://korrespondent.net/business/indexes/course_valjut/bank/";
         $link = "https://korrespondent.net/ajax/module.aspx?spm_id=553&fid=2&IsAjax=true";
 
-        $guzzleClient = new GuzzleClient();
 
-        $guzzleClient->request('GET', $link, ['headers', array(
-            'Proxy-Connection' => 'keep-alive',
-            'user-agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
-            'accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-            'pragma' => 'no-cache',
-            'cache-control' => 'no-cache',
-            'upgrade-insecure-requests' => '1',
-            'accept-encoding' => 'gzip, deflate, br',
-            'accept-language' => 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-            'Referer' => $link,
-        )]);
+        $html = $this->driver->getContent($link);
 
-        //dd(database_path());
-        $response = $guzzleClient->get($link);
-        $html = $response->getBody()->getContents();
-        //echo $html;
         $domCrawler = new DomCrawler($html);
         echo $domCrawler->html();
-        /*$table = $domCrawler->filter('table.indextable')->each(function ($item, $index, $arr){
-            echo '<p>'.$item->html().'</p>';
-        });*/
-        //echo $table->html();
-
-
     }
 
     public function second()
     {
+        $link = "https://korrespondent.net/business/indexes/course_valjut/bank/";
+        //$link = "https://korrespondent.net/ajax/module.aspx?spm_id=553&fid=2&IsAjax=true";
+        $html = $this->driver->getContent($link);
+        $data = $this->helper->prepareData($html);
 
-        //$link = "https://korrespondent.net/business/indexes/course_valjut/bank/";
-        $link = "https://korrespondent.net/ajax/module.aspx?spm_id=553&fid=2&IsAjax=true";
+        $course = new Course();
+        //$this->helper->saveCourse($data);
+        $courses = $this->helper->getAllCourses();
+        dump($data);
 
-        $guzzleClient = new GuzzleClient();
-
-        $guzzleClient->request('GET', $link, ['headers', array(
-            'Proxy-Connection' => 'keep-alive',
-            'user-agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
-            'accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-            'pragma' => 'no-cache',
-            'cache-control' => 'no-cache',
-            'upgrade-insecure-requests' => '1',
-            'accept-encoding' => 'gzip, deflate, br',
-            'accept-language' => 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-            'Referer' => $link,
-        )]);
-
-        $response = $guzzleClient->get($link);
-        $html = $response->getBody()->getContents();
-
-        $domCrawler = new DomCrawler($html);
-
-        $ru_date = $domCrawler->filter('.index-big-title .index-date');
-        echo 'ru_date => '.$ru_date->text().'<br>';
-         $currency = $domCrawler->filter('a.listbox__link');
-        echo 'currency => '.$currency->text().'<br>';
-
-        $table_rows = $domCrawler->filter('table.indextable>tbody>tr');
-
-        $table_rows->each(function (DomCrawler $node, $index){
-            echo 'Row number - '.$index.'<br>';
-            $cells = $node->filter('td');
-            $cells->each(function (DomCrawler $n, $i){
-                echo $i.' => '.$n->text().'<br>';
-            });
-            echo '<br>';
-        });
-
+        dd($courses);
     }
+
 }
