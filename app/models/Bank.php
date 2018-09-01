8cbba620d1c171e3485e0e70c33f0dca44390ccf
vladimir-pitchkurov <vladimir.pitchkurov@gmail.com>
2018-09-02 01:04:12 +0300

    bank id

--- app/models/Bank.php
+++ app/models/Bank.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bank extends Model
 {
-    //
+    public function bankId($title)
+    {
+        $title = trim($title);
+        $result = $this->where('title', $title)->first();
+
+        return !! $result ? $result->id : $this->insertGetId(['title' => $title]);
+    }
 }
