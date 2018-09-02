ff0af4b8b03d4b3abddb3bc9a1464c8e70240033
vladimir-pitchkurov <vladimir.pitchkurov@gmail.com>
2018-09-03 02:22:20 +0300

    change signature get all (date), add existing dates

--- app/models/Course.php
+++ app/models/Course.php
@@ -6,8 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
-    public function getAllCourses()
+    public function getAllCourses($params)
     {
-        return $this->leftJoin('banks', 'bank_id', '=', 'banks.id')->get();
+        $likeDate = false;
+        if($params){
+            $likeDate = $params['date-get'];
+            return $this->where('save_at', 'like', $likeDate.'%')->leftJoin('banks', 'bank_id', '=', 'banks.id')->get();
+        }else return $this->leftJoin('banks', 'bank_id', '=', 'banks.id')->get();
+    }
+
+    public function existingDates()
+    {
+        return $this
+            ->select('save_at', 'ru_date')
+            ->groupBy('ru_date')
+            ->orderBy('save_at', 'desc')
+            ->get();
     }
 }
